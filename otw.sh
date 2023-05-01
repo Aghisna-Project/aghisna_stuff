44f895c16cadf7da546e72a6c788dcd881f0e7cc
RooGhz720 <RooGhz720@gmail.com>
2023-05-01 18:01:03 +0800

    update script
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- otw.sh
+++ otw.sh
@@ -3,29 +3,34 @@
 # Created By Aghisna (RooGhz720)
 # Configuration for Bootlaggers rom
 
-
+echo "cloning dimulai"
 # cloning Device tree
 git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sweet.git -b btlg device/xiaomi/sweet
-sleep 10
-
+sleep 4
+echo "==================="
 # cloning Device tree common
 git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sm6150-common.git -b btlg device/xiaomi/sm6150-common
-sleep 10
-
+sleep 4
+echo "==================="
 # cloning Vendor tree
 git clone --depth=1 https://github.com/Aghisna-Project/proprietary_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
-sleep 10
-
+sleep 4
+echo "==================="
 # cloning Vendor tree common
 git clone --depth=1 https://github.com/Aghisna-Project/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
-sleep 10
-
+sleep 4
+echo "==================="
 # cloning kernel tree
 git clone --depth=1 https://github.com/Aghisna-Project/kernel_xiaomi_sm6150.git -b aghisna-13 kernel/xiaomi/sm6150
-sleep 10
-
-# cloning miui or leica cam
+sleep 4
+echo "==================="
+# cloning leica cam
 git clone --depth=1 https://gitlab.com/unsatifsed27/miuicamera.git -b tosle vendor/xiaomi/sweet-leica
-sleep 10
-
+sleep 4
+echo "==================="
+# cloning some stuff
+git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-playground
+git clone --depth=1 https://github.com/Aghisna-Project/android_hardware_xiaomi hardware/xiaomi
+sleep 4
+echo " "
 echo "cloning selesai untuk rom bootleggers"
