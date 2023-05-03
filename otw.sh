29940ef34902319d32cdabb77dcea6896e937d12
RooGhz720 <RooGhz720@gmail.com>
2023-05-03 23:51:00 +0800

    add google clang 17.0
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- otw.sh
+++ otw.sh
@@ -29,10 +29,14 @@ git clone --depth=1 https://gitlab.com/unsatifsed27/miuicamera.git -b tosle vend
 sleep 4
 echo "==================="
 # cloning some stuff
-git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-playground
+# git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-playground
+git clone --depth=1 https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747 -b master prebuilts/clang/host/linux-x86/clang-playground
 git clone --depth=1 https://github.com/Aghisna-Project/android_hardware_xiaomi hardware/xiaomi
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage
 git clone --depth=1 https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings
+
+# cloning some app needed
+git clone https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
 sleep 4
 echo " "
 echo "cloning selesai untuk rom bootleggers"
