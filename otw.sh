6997d29b2151102a0b37a825fd95102671c5626d
RooGhz720 <RooGhz720@gmail.com>
2023-05-27 15:00:10 +0800

    update btlg
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- otw.sh
+++ otw.sh
@@ -5,11 +5,11 @@
 
 echo "cloning dimulai"
 # cloning Device tree
-git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sweet.git -b btlg device/xiaomi/sweet
+git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sweet.git -b btlg-new device/xiaomi/sweet
 sleep 4
 echo "==================="
 # cloning Device tree common
-git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sm6150-common.git -b btlg device/xiaomi/sm6150-common
+git clone --depth=1 https://github.com/Aghisna-Project/android_device_xiaomi_sm6150-common.git -b btlg-new device/xiaomi/sm6150-common
 sleep 4
 echo "==================="
 # cloning Vendor tree
@@ -17,7 +17,7 @@ https://github.com/Aghisna-Project/vendor_xiaomi.git -b btlg device/xiaomi
 sleep 4
 echo "==================="
 # cloning kernel tree
-git clone --depth=1 https://github.com/Aghisna-Project/kernel_xiaomi_sm6150.git -b aghisna-13 kernel/xiaomi/sm6150
+git clone --depth=1 https://github.com/Aghisna-Project/aghisna.git -b aghisna-13 kernel/xiaomi/sm6150
 sleep 4
 echo "==================="
 # cloning leica cam
@@ -25,8 +25,8 @@ git clone --depth=1 https://gitlab.com/unsatifsed27/miuicamera.git -b tosle vend
 sleep 4
 echo "==================="
 # cloning some stuff
-# git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-playground
-git clone --depth=1 https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747 -b master prebuilts/clang/host/linux-x86/clang-playground
+git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang -b master prebuilts/clang/host/linux-x86/clang-playground
+# git clone --depth=1 https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747 -b master prebuilts/clang/host/linux-x86/clang-playground
 git clone --depth=1 https://github.com/Aghisna-Project/android_hardware_xiaomi hardware/xiaomi
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage
 git clone --depth=1 https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings
